de88acfaf06f6cdddf0f1c5030eb1262eb6e02e3
zyl <352439694@qq.com>
2019-04-09 10:41:40 +0800

    init

--- scripts/test_search.py
+++ scripts/test_search.py
@@ -4,16 +4,16 @@ sys.path.append(os.getcwd())
 sys.setdefaultencoding('utf-8')
 from Page.Page import Page_Obj
 from Base.InitDiver import init_driver
-#from Base.Read_Data import ret_yaml_data
-#import pytest
+from Base.Read_Data import ret_yaml_data
+import pytest
 
 
-# def yaml_data():
-#     data_list = []
-#     data = ret_yaml_data("search_data").get("Search_Data")
-#     for i in data.keys():
-#         data_list.append((i, data.get(i).get("test"), data.get(i).get("expect_data")))
-#     return data_list
+def yaml_data():
+    data_list = []
+    data = ret_yaml_data("search_data").get("Search_Data")
+    for i in data.keys():
+        data_list.append((i, data.get(i).get("test"), data.get(i).get("expect_data")))
+    return data_list
 
 
 class Test_Search_Page:
@@ -27,13 +27,13 @@ class Test_Search_Page:
         self.search_obj.search_return()
         self.driver.quit()
 
-    #@pytest.mark.parametrize("test_num,text,expect_data", yaml_data())
+    @pytest.mark.parametrize("test_num,text,expect_data", yaml_data())
     #@pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     @allure.step('测试搜索')
     @allure.issue('http://www.baidu.com','Test')
-    def test_input_text(self):
-        print("用例编号:")
-        allure.attach('描述',"执行测试步骤")
+    def test_input_text(self, test_num, text, expect_data):
+        print("用例编号:", test_num)
+        allure.attach('描述',"执行测试步骤"+test_num)
         self.search_obj.search_input("你好")
-        #self.driver.get_screenshot_as_file("./screen/%s.png" % )
-        # assert expect_data == 456
+        self.driver.get_screenshot_as_file("./screen/%s.png" % test_num)
+        assert expect_data == 456
